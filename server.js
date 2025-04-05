8f71e84ee2aa6126694f579eb0b23ed6e2dc592a
griffmass <griffmassdestroyer@gmail.com>
2025-04-05 16:53:39 +0800

    Added express

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Default route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
